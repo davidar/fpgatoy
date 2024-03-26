96fe86a46c88a876a9b9cbd97d1d23607bd0cba3
David A Roberts <d@vidr.cc>
2024-03-26 20:35:03 +1000

    add sim

--- fpgatoy.py
+++ fpgatoy.py
@@ -8,8 +8,12 @@ from litex.gen import LiteXModule
 
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.interconnect import stream
-from litex.soc.cores.video import VideoHDMIPHY, VideoTimingGenerator
+from litex.soc.cores.video import VideoGenericPHY, VideoHDMIPHY, VideoTimingGenerator
 from litex.soc.cores.video import video_data_layout, video_timing_layout
+
+from litex.build.io import CRG
+
+import litex.tools.litex_sim as sim
 
 
 # Boilerplate based on litex.soc.cores.video.ColorBarsPattern
@@ -82,6 +86,57 @@ class BaseSoC(SoCCore):
         if res is not None:
             pattern.comb += res
         pattern = ClockDomainsRenamer("hdmi")(pattern)
+        self.add_module("pattern", pattern)
+
+        # platform.add_source_dir(path="./")
+
+        self.comb += [
+            pattern_vtg.source.connect(pattern.vtg_sink),
+            pattern.source.connect(self.videophy.sink),
+        ]
+
+
+class SimSoC(SoCCore):
+    def __init__(self, main_image):
+        platform = sim.Platform()
+        video_timing = "640x480@60Hz"
+        sys_clk_freq = int(25e6)
+
+        # led = platform.request("user_led_n", 0)
+        # counter = Signal(26)
+        # self.comb += led.eq(counter[25])
+        # self.sync += counter.eq(counter + 1)
+
+        # self.crg = _CRG(
+        #     platform,
+        #     sys_clk_freq,
+        #     with_video_pll=True,
+        #     pix_clk=25e6,  # video_timings[video_timing]["pix_clk"]
+        # )
+
+        self.crg = CRG(platform.request("sys_clk"))
+
+        SoCCore.__init__(
+            self,
+            platform,
+            int(sys_clk_freq),
+            ident="LiteX Simulation",
+            cpu_type=None,
+            with_uart=False,
+        )
+
+        # self.videophy = VideoHDMIPHY(platform.request("gpdi"), clock_domain="hdmi")
+        self.submodules.videophy = VideoGenericPHY(platform.request("vga"))
+
+        pattern_vtg = VideoTimingGenerator(default_video_timings=video_timing)
+        # pattern_vtg = ClockDomainsRenamer("hdmi")(pattern_vtg)
+        self.add_module("pattern_vtg", pattern_vtg)
+
+        pattern = Pattern()
+        res = main_image(pattern, platform)
+        if res is not None:
+            pattern.comb += res
+        # pattern = ClockDomainsRenamer("hdmi")(pattern)
         self.add_module("pattern", pattern)
 
         # platform.add_source_dir(path="./")
