59a71c9b4d452bebf0b50e4cf0012f75c0b6fbd0
David A Roberts <d@vidr.cc>
2024-03-28 19:09:56 +1000

    connect over uart

--- fpgatoy.py
+++ fpgatoy.py
@@ -1,9 +1,12 @@
+import time
+
 from migen import *
 from migen.genlib.cdc import MultiReg
 
 from litex_boards.platforms import colorlight_i5
 from litex_boards.targets.colorlight_i5 import _CRG as colorlight_i5_CRG
 
+from litex import RemoteClient
 from litex.gen import LiteXModule
 
 from litex.soc.integration.builder import Builder
@@ -11,12 +14,18 @@ from litex.soc.integration.soc_core import SoCMini
 from litex.soc.interconnect import stream
 from litex.soc.cores.video import VideoHDMIPHY, VideoTimingGenerator, VideoVGAPHY
 from litex.soc.cores.video import video_data_layout, video_timing_layout
+from litex.soc.cores.uart import UARTWishboneBridge
+from litex.soc.cores import gpio
 
 from litex.build.generic_platform import GenericPlatform
 from litex.build.io import CRG
 from litex.build.sim.config import SimConfig
 
 import litex.tools.litex_sim as sim
+
+
+class Led(gpio.GPIOOut):
+    pass
 
 
 # Boilerplate based on litex.soc.cores.video.ColorBarsPattern
@@ -61,7 +70,13 @@ class BaseSoC(SoCMini):
         clock_domain="sys",
         video_timing="640x480@60Hz",
     ):
-        SoCMini.__init__(self, platform, sys_clk_freq)
+        SoCMini.__init__(
+            self,
+            platform,
+            sys_clk_freq,
+            ident="fpgatoy SoC",
+            ident_version=True,
+        )
 
         try:
             self.videophy = VideoHDMIPHY(platform.request("gpdi"), clock_domain)
@@ -87,10 +102,27 @@ class BaseSoC(SoCMini):
 
 class MySoC(BaseSoC):
     def __init__(self, main_image):
+        sys_clk_freq = int(25e6)
         platform = colorlight_i5.Platform("i9", "7.2")
-        self.crg = colorlight_i5_CRG(platform, 25e6, with_video_pll=True, pix_clk=25e6)
-        BaseSoC.__init__(self, main_image, platform, 25e6, "hdmi")
-        self.blink()  # optional
+        self.crg = colorlight_i5_CRG(
+            platform, sys_clk_freq, with_video_pll=True, pix_clk=25e6
+        )
+        BaseSoC.__init__(self, main_image, platform, sys_clk_freq, "hdmi")
+
+        ### OPTIONAL ###
+
+        # self.blink()
+
+        # No CPU, use Serial to control Wishbone bus
+        self.submodules.serial_bridge = UARTWishboneBridge(
+            platform.request("serial"), sys_clk_freq
+        )
+        self.bus.add_master(master=self.serial_bridge.wishbone)
+
+        # Led
+        user_leds = Cat(*[platform.request("user_led_n", i) for i in range(1)])
+        self.submodules.leds = Led(user_leds)
+        self.add_csr("leds")
 
     def blink(self):
         led = self.platform.request("user_led_n", 0)
@@ -99,11 +131,36 @@ class MySoC(BaseSoC):
         self.sync += counter.eq(counter + 1)
 
     def run(self):
-        builder = Builder(self)
+        builder = Builder(self, csr_csv="csr.csv")
         builder.build(compress=True)
 
         prog = self.platform.create_programmer()
         prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+        self.connect()
+
+    def connect(self):
+        wb = RemoteClient()
+        wb.open()
+
+        fpga_id = ""
+        for i in range(256):
+            c = chr(wb.read(wb.bases.identifier_mem + 4 * i) & 0xFF)
+            fpga_id += c
+            if c == "\0":
+                break
+        print("Connected to UART:", fpga_id)
+
+        while True:
+            try:
+                wb.regs.leds_out.write(0)
+                time.sleep(0.1)
+                wb.regs.leds_out.write(1)
+                time.sleep(0.1)
+            except KeyboardInterrupt:
+                break
+
+        wb.close()
 
 
 class SimSoC(BaseSoC):
